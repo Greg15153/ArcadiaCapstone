027dc4e36cd80dca70449edfec52c2b22d98202d
Greg Ellis - Main Desktop <gellis@arcadia.edu>
2014-09-07 17:17:17 -0400

    Initial commit of Project
    Here goes our actual project, learning / work in progress. Currently
    outputting results. No connection to any database yet.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UserController : ApiController
+    {
+        UserContext db = new UserContext();
+
+        // If user goes to a page that isn't supported, shoot not found...
+        // GET /User
+        public IHttpActionResult Get()
+        {
+            return Json(new Error("MethodNotFound"));
+        }
+        public IHttpActionResult Post()
+        {
+            User greg = new User { Id = 0, Username = "Greg", Password = "Pass", Admin = 0 };
+            db.Users.Add(greg);
+            db.SaveChanges();
+
+            return Json("Success");
+        }
+        // Gets a user's information based off ID, if no/incorrect apiKey shoot errors, if not return user data
+        // GET /User/id?apiKey=
+        public IHttpActionResult GetUser(int id, string apiKey = "")
+        {
+
+            if (apiKey == "")
+                return Json(new Error("MissingAPI"));
+            else if(apiKey != "12345")
+                return Json(new Error("UnAuthorized"));
+
+            User user = new User();
+     
+            switch (id)
+            {
+                case 0:
+                    user.Id = 0;
+                    user.Username = "Greg";
+                    user.Password = "Password123";
+                    user.Admin = 1;
+                    break;
+                case 1:
+                    user.Id = 1;
+                    user.Username = "Mike";
+                    user.Password = "Password123";
+                    user.Admin = 0;
+                    break;
+                default:
+                    return Json(new Error("ResourceNotFound"));
+            }
+
+            return Json(user);
+        }
+
+
+    }
+}
---
+++ WebAPI/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace WebAPI.Models
+{
+
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Admin { get; set; }
+    }
+
+}
